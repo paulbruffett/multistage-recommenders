88588b5fb919b9e9b1633904b3250c22c7c2832d
Paul Bruffett <paulbruffett@protonmail.com>
2023-01-02 15:25:26 -0800

    updated groups

--- cli/setup.sh
+++ cli/setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azure-ml"
+GROUP="mlworkspace"
 LOCATION="West US"
-WORKSPACE="recommend-pb-workspace"
+WORKSPACE="mlworkspace"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
