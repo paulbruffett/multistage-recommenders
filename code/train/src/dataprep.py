da99975e60853fb272362ef1a0352d82bb591794
Paul Bruffett <paulbruffett@protonmail.com>
2023-01-04 19:19:28 -0800

    try reading files

--- code/train/src/dataprep.py
+++ code/train/src/dataprep.py
@@ -12,21 +12,21 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 #blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-account_url = "https://mlworkspace3685223142.blob.core.windows.net"
-default_credential = DefaultAzureCredential()
+#account_url = "https://mlworkspace3685223142.blob.core.windows.net"
+#default_credential = DefaultAzureCredential()
 
 # Create the BlobServiceClient object
-blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+#blob_service_client = BlobServiceClient(account_url, credential=default_credential)
 
-cclient = blob_service_client.get_container_client("azureml-blobstore-64879d37-44cb-403b-b850-a7bc6761c022")
+#cclient = blob_service_client.get_container_client("azureml-blobstore-64879d37-44cb-403b-b850-a7bc6761c022")
 
+print(os.listdir("."))
 
-
-blob_stream = cclient.download_blob("aliccp_train/common_features_train.csv")
+#blob_stream = cclient.download_blob("aliccp_train/common_features_train.csv")
 with open(file="common_features_train.csv", mode="wb") as download_file:
     download_file.write(blob_stream.readall())
 
-blob_stream = cclient.download_blob("aliccp_train/sample_skeleton_train.csv.csv")
+#blob_stream = cclient.download_blob("aliccp_train/sample_skeleton_train.csv.csv")
 with open(file="sample_skeleton_train.csv.csv", mode="wb") as download_file:
     download_file.write(blob_stream.readall())
 
