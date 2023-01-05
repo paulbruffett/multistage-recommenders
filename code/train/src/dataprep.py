219b132dae12b5352a4ed827364ea94118ee7078
Paul Bruffett <paulbruffett@protonmail.com>
2023-01-04 19:06:47 -0800

    update

--- code/train/src/dataprep.py
+++ code/train/src/dataprep.py
@@ -8,11 +8,18 @@ from tqdm import tqdm
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-connect_str = os.getenv('CONNECTION_STRING')
+#connect_str = os.getenv('CONNECTION_STRING')
 
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+#blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+account_url = "https://mlworkspace3685223142.blob.core.windows.net"
+default_credential = DefaultAzureCredential()
+
+# Create the BlobServiceClient object
+blob_service_client = BlobServiceClient(account_url, credential=default_credential)
 
 cclient = blob_service_client.get_container_client("azureml-blobstore-64879d37-44cb-403b-b850-a7bc6761c022")
+
 
 
 blob_stream = cclient.download_blob("aliccp_train/common_features_train.csv")
