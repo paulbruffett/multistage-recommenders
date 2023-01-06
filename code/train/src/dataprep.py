5bc9a4511ff9f7a229f77f45f70067015eaf0685
Paul Bruffett <paulbruffett@protonmail.com>
2023-01-05 21:01:40 -0800

    updated file output

--- code/train/src/dataprep.py
+++ code/train/src/dataprep.py
@@ -5,29 +5,7 @@ import re
 import numpy as np
 from tqdm import tqdm
 import argparse
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-#connect_str = os.getenv('CONNECTION_STRING')
-
-#blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-#account_url = "https://mlworkspace3685223142.blob.core.windows.net"
-#default_credential = DefaultAzureCredential()
-
-# Create the BlobServiceClient object
-#blob_service_client = BlobServiceClient(account_url, credential=default_credential)
-
-#cclient = blob_service_client.get_container_client("azureml-blobstore-64879d37-44cb-403b-b850-a7bc6761c022")
-
-
-#blob_stream = cclient.download_blob("aliccp_train/common_features_train.csv")
-"""with open(file="common_features_train.csv", mode="wb") as download_file:
-    download_file.write(blob_stream.readall())
-
-#blob_stream = cclient.download_blob("aliccp_train/sample_skeleton_train.csv.csv")
-with open(file="sample_skeleton_train.csv.csv", mode="wb") as download_file:
-    download_file.write(blob_stream.readall())"""
 
 def parse_args():
     # setup arg parser
@@ -53,6 +31,7 @@ if __name__ == "__main__":
     
     print(path)
     print(os.listdir(path))
+    os.makedirs(path+"/train_processed/", exist_ok=True)
 
 
     def _convert_common_features(common_path, pickle_path=None):
@@ -99,7 +78,6 @@ if __name__ == "__main__":
                 index = int((i / file_size) - 1)
                 file_name = f"train_{index}.parquet"
                 df.to_parquet(
-                    os.path.join(file_name)
+                    os.path.join([path,"train_processed", file_name])
                 )
-                c_client.upload_blob("aliccp/processed/"+file_name,file_name)
                 current = []
