fd31672168fa446902052f5b43b130cafeadbd21
Paul Bruffett <paulbruffett@protonmail.com>
2023-03-01 19:07:17 -0800

    updated data filtering

--- code/train/src/algo_training.py
+++ code/train/src/algo_training.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 import mlflow
 import tensorflow_recommenders as tfrs
 import datetime
+import gc
 
 
 def parse_args():
@@ -26,6 +27,17 @@ def parse_args():
     # return args
     return args
 
+def dataset_to_tensor(df):
+    features = ['item_id','user_id']
+    for i in features:
+        df[i] = pd.to_numeric(df[i], errors='coerce',downcast="integer")
+
+    df_features = df[df['click']==1][['item_id','user_id']]
+    df_features= df_features.dropna()
+    df_tensor = tf.convert_to_tensor(df_features, dtype=tf.int64)
+
+    f_dataset = tf.data.Dataset.from_tensor_slices(df_tensor)
+    return f_dataset, df_features
 
 # run script
 if __name__ == "__main__":
@@ -40,30 +52,26 @@ if __name__ == "__main__":
     # run main function
     path = args.file_location
     log_path = args.output_location
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path+"/logs/"+now, profile_batch=(1,5))
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path+"/logs/"+now)
 
-    df = pd.read_parquet(path+'/train_processed/')
+    train = pd.read_parquet(path+'/train_processed/')
 
-    print(len(df))
 
-    features = ['item_id','user_id']
-    for i in features:
-        df[i] = pd.to_numeric(df[i], errors='coerce',downcast="integer")
+    #filter out long tail products and reduce size of dataset
+    train['counter'] = 1
+    df_agg = train[['item_id','counter']].groupby('item_id').count()
+    df_agg = df_agg[df_agg['counter']>500]
+    train = pd.merge(df_agg, train, on=['item_id', 'item_id'], how='left')
+    print(len(train))
 
-    df_features = df[df['click']==1][['item_id','user_id']]
-    df_features= df_features.dropna()
-    df_tensor = tf.convert_to_tensor(df_features, dtype=tf.int64)
+    train, df_features = dataset_to_tensor(train)
 
-    f_dataset = tf.data.Dataset.from_tensor_slices(df_tensor)
-
-    len_train = round(len(f_dataset)*.2)
-    len_test = round(len(f_dataset)*.2)
 
     tf.random.set_seed(42)
-    shuffled = f_dataset.shuffle(len(f_dataset), seed=42, reshuffle_each_iteration=False)
+    train = train.shuffle(len(train), seed=42, reshuffle_each_iteration=False)
 
-    train = shuffled.take(len_train)
-    test = shuffled.skip(len_train).take(len_test)
+    
+    #train = train.take(round(len(train)*0.01))
 
     unique_items = np.unique(df_features['item_id'].values.squeeze())
     unique_users = np.unique(df_features['user_id'].values.squeeze())
@@ -73,7 +81,7 @@ if __name__ == "__main__":
 
     item_ids = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(df_features['item_id'].values,dtype=tf.int64))
 
-    embedding_dimension = 32
+    embedding_dimension = 64
     mlflow.log_param("embedding_dimension", embedding_dimension)
 
     user_model = tf.keras.Sequential([
@@ -94,7 +102,7 @@ if __name__ == "__main__":
     task = tfrs.tasks.Retrieval(metrics=metrics)
 
     class RetrievalModel(tfrs.Model):
-        def __init__(self, user_model, movie_model):
+        def __init__(self, user_model, item_model):
             super().__init__()
             self.item_model: tf.keras.Model = item_model
             self.user_model: tf.keras.Model = user_model
@@ -105,10 +113,10 @@ if __name__ == "__main__":
             user_embeddings = self.user_model(features['user_id'])
             # And pick out the movie features and pass them into the movie model,
             # getting embeddings back.
-            positive_movie_embeddings = self.item_model(features['item_id'])
+            positive_item_embeddings = self.item_model(features['item_id'])
 
             # The task computes the loss and the metrics.
-            return self.task(user_embeddings, positive_movie_embeddings)
+            return self.task(user_embeddings, positive_item_embeddings)
         
     model = RetrievalModel(user_model, item_model)
     model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1))
@@ -119,6 +127,32 @@ if __name__ == "__main__":
         "item_id": x[0],
     })
 
-    cached_train = train_dataset.batch(8192).cache()
+    cached_train = train_dataset.batch(512).cache()
 
-    model.fit(cached_train, epochs=1,callbacks=[tensorboard_callback])
+    model.fit(cached_train, epochs=15,callbacks=[tensorboard_callback])
+
+    del df_features,train,train_dataset,cached_train
+    gc.collect()
+
+    test = pd.read_parquet(path+'/test_processed/')
+    test = pd.merge(df_agg, test, on=['item_id', 'item_id'], how='left')
+    test, _ = dataset_to_tensor(test)
+    #test = test.take(round(len(test)*0.01))
+
+    test_dataset = test.map(lambda x: {
+        "user_id": x[1],
+        "item_id": x[0],
+    })
+
+    cached_test = test_dataset.batch(512).cache()
+
+    model.evaluate(cached_test, return_dict=True, callbacks=[tensorboard_callback])
+
+    index = tfrs.layers.factorized_top_k.BruteForce(model.user_model,k = 50)
+
+    index.index_from_dataset(tf.data.Dataset.zip((item_ids.batch(100), item_ids.batch(100).map(model.item_model))))
+
+    _, titles = index(tf.constant([42]))
+    print(titles)
+    tf.saved_model.save(index, "index")
+    mlflow.log_artifacts("index", "index")
